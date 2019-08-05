36e6e26bc70253aa8835c1d6c87c8bfd4374b574
Alex <alxford45@gmail.com>
2019-08-05 12:20:01 -0500

    added testing endpoint 'api/hello'

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 const PORT = 3000;
+app.get("/api/hello", (req, res) => {
+  console.log(`req recieved: ${req.url}`);
+  res.send("hello");
+});
 app.listen(PORT, () => {
-  console.log(`now listening on port ${PORT}\nhttp://localhost/${PORT}`);
+  console.log(`now listening on port ${PORT}`);
 });
