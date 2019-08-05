cd06aae0fa59e2923c5676ffa56797d893da6387
Alex <alxford45@gmail.com>
2019-08-05 11:30:29 -0500

    added simple express boilerplate

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`now listening on port ${PORT}\nhttp://localhost/${PORT}`);
+});
