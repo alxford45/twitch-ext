c4a2030c8b1c966392fd8a7e4ab4ee8177f54800
Alex <alxford45@gmail.com>
2019-08-14 17:33:47 -0500

    temporary prototyping of server modules

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,57 @@
 import express from "express";
-
+import { concatMap, map, tap } from "rxjs/operators";
+import { meta } from "./modules/meta";
+import { createWebSocket } from "./modules/createWebSocket";
+import { fetchData } from "./fetchData";
+import { AxiosRequestConfig } from "axios";
 const app = express();
 const PORT = 3000;
-app.get("/api/hello", (req, res) => {
-  console.log(`req recieved: ${req.url}`);
-  res.send("hello");
+const socketUrl = "wss://api.runelite.net/ws";
+
+app.get("/api/auth", (_, res) => {
+  res.send("testing");
 });
 app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`);
+  const request = meta.pipe(
+    concatMap(args => {
+      const { uid, version } = args;
+      const options: AxiosRequestConfig = {
+        headers: {
+          "RUNELITE-AUTH": uid
+        },
+        method: "GET",
+        url: `http://api.runelite.net/${version}/loottracker`
+      };
+      const response = fetchData<any>(options)!;
+      return response;
+    })
+  );
+  const session$ = meta.pipe(
+    tap(x => {
+      console.log(x.oauthUrl);
+    }),
+    map(args => args.uid),
+    concatMap(uid => {
+      const socket$ = createWebSocket(socketUrl, uid);
+      return socket$;
+    })
+  );
+  session$.subscribe({
+    next: val => console.log(val),
+    error: err => console.log(err),
+    complete: () => {
+      console.log("done");
+      request.subscribe(x => {
+        const data = x.map((y: any) => {
+          return {
+            target: `${y.type} ${y.eventId}`,
+            drops: y.drops.flatMap((z: any) => `id: ${z.id}, qty: ${z.qty}`),
+            time: y.time.seconds
+          };
+        });
+        console.log(data);
+      });
+    }
+  });
 });
