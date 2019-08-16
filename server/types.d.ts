646ec0a3df09cbdda71476d5f1e23e6ee8092a9b
Alex <alxford45@gmail.com>
2019-08-15 22:10:18 -0500

    small formatting fix

--- server/types.d.ts
+++ server/types.d.ts
@@ -4,6 +4,7 @@ import WebSocket, { MessageEvent, OpenEvent, CloseEvent, ErrorEvent } from "ws";
  * tap name.
  *
  * endpoint: GET /repos/:owner/:repo/releases/tags/:tag
+ *
  * source: https://developer.github.com/v3/repos/releases/#get-a-release-by-tag-name
  *
  */
@@ -12,11 +13,13 @@ export declare type GithubResponse = [{ name: string }];
  * The explicit schema of the Runelite response for loggin in.
  *
  * endpoint: GET /api.runelite.net/runelite-:version/account/login
+ *
  * source: https://static.runelite.net/api/http-service/#accountlogin
  */
 export declare type RuneliteAuthResponse = { uid: string; oauthUrl: string };
 /**
- * Union type of ws [[WebSocket]] Events:
+ * Union type of ws [[WebSocket]] Events
+ *
  * [[MessageEvent]], [[OpenEvent]], [[CloseEvent]], [[ErrorEvent]]
  */
 export declare type WebSocketEvent =
