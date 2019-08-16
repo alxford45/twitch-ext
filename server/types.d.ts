385db96edd018ed7959b5c0cd484811d63393e06
Alex <alxford45@gmail.com>
2019-08-15 22:57:12 -0500

    added additional typings with documentation

--- server/types.d.ts
+++ server/types.d.ts
@@ -18,6 +18,34 @@ export declare type GithubResponse = [{ name: string }];
  */
 export declare type RuneliteAuthResponse = { uid: string; oauthUrl: string };
 /**
+ * The explicit schema of Runelite response for LootTracker.
+ *
+ * endpoint: GET /api.runelite.net/runelite-:version/loottracker
+ *
+ * source: https://static.runelite.net/api/http-service/#loottracker
+ */
+export declare type RuneliteLootTrackerResponse = LootRecord[];
+/**
+ * The excplicit schema of Runelite LootRecord Object
+ *
+ * source: https://static.runelite.net/api/http-service/#/definitions/LootRecord
+ */
+export declare type LootRecord = {
+  eventId: string;
+  type: string;
+  drops: GameItem[];
+  time: number;
+};
+/**
+ * The explicit schema of Runelite GameItem Object
+ *
+ * source: https://static.runelite.net/api/http-service/#/definitions/GameItem
+ */
+export declare type GameItem = {
+  id: number;
+  qty: number;
+};
+/**
  * Union type of ws [[WebSocket]] Events
  *
  * [[MessageEvent]], [[OpenEvent]], [[CloseEvent]], [[ErrorEvent]]
