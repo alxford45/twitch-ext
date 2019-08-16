3a87357c53285ff9e8f601dd2f22865d3d2fd0d2
Alex <alxford45@gmail.com>
2019-08-15 20:53:13 -0500

    added documentation

--- server/modules/auth.ts
+++ server/modules/auth.ts
@@ -4,10 +4,23 @@ import { concatMap } from "rxjs/operators";
 import uuid from "uuid/v4";
 
 import { fetchData } from "../fetchData";
-import { RuneliteResponse } from "../types";
+import { RuneliteAuthResponse } from "../types";
 import { version$ } from "./version";
-
-export const auth$: Observable<RuneliteResponse> = version$.pipe(
+/**
+ * [[Observable]] created by concatinating the observable stream of [[version$]] and returning
+ * a new observable from the response of [[fetchData]] to emit the uuid and oauthURL required for
+ * authenticating a Runelite User.
+ *
+ * OauthURL is the google authentication url used by Runelite for users to "sign-in" to their
+ * Runelite account.
+ *
+ * Uid is the a locally generated uuid that is used in the header to authenticate all future Runelite Requests.
+ *
+ * The uuid is "authenticated" by creating a websocket connection to Runelite with the uuid as the session id.
+ * If the user is authenticated by runelite (by "signing in" with their Gmail), Runelite will send a success message
+ * through the websocket and allow future requests with that uuid.
+ */
+export const auth$: Observable<RuneliteAuthResponse> = version$.pipe(
   concatMap((v: string) => {
     const localUuid = uuid();
     const options: AxiosRequestConfig = {
@@ -18,7 +31,7 @@ export const auth$: Observable<RuneliteResponse> = version$.pipe(
       url: `http://api.runelite.net/${v}/account/login?uuid=${localUuid}`
     };
 
-    const response = fetchData<RuneliteResponse>(options)!;
+    const response = fetchData<RuneliteAuthResponse>(options)!;
     return response;
   })
 );
--- server/modules/meta.ts
+++ server/modules/meta.ts
@@ -4,15 +4,31 @@ import { map } from "rxjs/operators";
 import { auth$ } from "./auth";
 import { version$ } from "./version";
 
+/**
+ * [[Observable]]<version: string, oauthUrl: string, uid: string> created by combining the
+ * observables [[version$]] (observable<string>) and [[auth$]] (observable<[[RuneliteAuthResponse]]>)
+ * using [[combineLatest]] to emit the latest values of version, oauthUrl and uid.
+ *
+ * Version and uid will be used on all future request, and oauthUrl will be needed for initial authentication.
+ */
 const source$ = combineLatest(version$, auth$).pipe(
   map(args => {
     const [version, { oauthUrl, uid }] = args;
     return { version: version, oauthUrl: oauthUrl, uid: uid };
   })
 );
+/**
+ * Meta is exported as a [[ReplaySubject]] from [[source$]] so that all future request after authentication have
+ * access to the latest emitted value. By wrapping the source observable in a replay subject, all future subscriptions
+ * will NOT recalculate the uid value. This is necessary because only uid value authenticated will be valid.
+ */
 export const meta = new ReplaySubject<{
   version: string;
   oauthUrl: string;
   uid: string;
 }>(1);
+/**
+ * source$ is immediately subscribed to meta so that all future subscriptions of meta are initialized
+ * with the source$ subscription.
+ */
 source$.subscribe(meta);
