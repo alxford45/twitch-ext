63e2ac5caa19b2c8cd5d3afa89db9e516ee99202
Alex <alxford45@gmail.com>
2019-08-23 11:42:09 -0500

    refactored webpack configs

--- config/config.ts
+++ config/config.ts
@@ -1,27 +1,43 @@
+import nodemon from "config/nodemon";
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import { DevServer, RuleSetRule, Mode } from "./config.types";
+import { RuleSetUseItem } from "webpack";
+import { Configuration as DevServer } from "webpack-dev-server";
+import { Dirname as dirname, Path as path } from "../path";
 
-export const MODE: Mode = "development";
-export const NAME: string = "web";
-export const ENTRY: string = "./web/index.ts";
+export const types = path.resolve(dirname, "types");
 
+export const web = {
+  name: "web",
+  entry: "./web/index.ts"
+};
+export const server = {
+  name: "server",
+  entry: "./server/index.ts",
+  context: path.resolve(dirname)
+};
 export const devServer: DevServer = {
   host: "localhost",
   port: 8080,
   hot: false,
-  filename: "main.js",
   proxy: {
     "**": "http://localhost:3000"
   }
 };
-export const TSLoaderRules: RuleSetRule = {
-  test: /\.ts?$/,
-  use: "ts-loader",
-  exclude: /node_modules/,
-  resolve: {
-    extensions: [".ts", ".js"]
+
+export const BabelLoader: RuleSetUseItem = {
+  loader: "babel-loader",
+  options: {
+    sourceType: "unambiguous"
   }
 };
+export const TSLoader: RuleSetUseItem = {
+  loader: "ts-loader"
+};
+
+const NodemonPlugin = require("nodemon-webpack-plugin") as nodemon.WebpackPlugin;
+export const nodemonPlugin = new NodemonPlugin({
+  script: "dist/main.js"
+});
 export const htmlWebpackPlugin: HtmlWebpackPlugin = new HtmlWebpackPlugin({
   template: "./web/index.html"
 });
--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,22 +1,64 @@
-import { Configuration } from "./config.types";
+import nodeExternals from "webpack-node-externals";
+import { Configuration } from "webpack";
+
 import {
-  ENTRY,
-  MODE,
-  NAME,
-  TSLoaderRules,
+  server,
+  web,
   devServer,
-  htmlWebpackPlugin
+  types,
+  nodemonPlugin,
+  htmlWebpackPlugin,
+  BabelLoader,
+  TSLoader
 } from "./config";
 
-const config: Configuration = {
-  name: NAME,
-  mode: MODE,
-  entry: ENTRY,
-  devServer: devServer,
-  module: {
-    rules: [TSLoaderRules]
+const configuration: Configuration[] = [
+  {
+    name: server.name,
+    entry: server.entry,
+    context: server.context,
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.ts|.js$/,
+          exclude: /node_modules/,
+          use: [BabelLoader, TSLoader]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+      alias: {
+        types: types
+      }
+    },
+    target: "node",
+    externals: [nodeExternals()],
+    plugins: [nodemonPlugin]
   },
-  plugins: [htmlWebpackPlugin]
-};
+  {
+    name: web.name,
+    entry: web.entry,
+    mode: "development",
+    devServer: devServer,
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)|.js(x?)$/,
+          exclude: /node_modules/,
+          use: [BabelLoader, TSLoader]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      alias: {
+        types: types
+      }
+    },
+    plugins: [htmlWebpackPlugin]
+  }
+];
 
-export default config;
+export default configuration;
